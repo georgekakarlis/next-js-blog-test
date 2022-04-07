86598dfe1f4212bc8b51c982f3911858d1f8e8d6
George K <georgekakarlis@yahoo.com>
2022-04-08 00:32:15 +0300

    #2 commit

---
+++ components/About.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">hello</p>
+      <p className="text-center">can u read me</p>
+    </div>
+  );
+};
+
+export default About;
